3e214e95138ad0903ffb1173ea5168af0e030bd0
Casper Fenger Jensen <43348526+Vimian@users.noreply.github.com>
2024-05-06 18:02:10 +0200

    save

--- assignment_2/src/main/main.c
+++ assignment_2/src/main/main.c
@@ -4,67 +4,64 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "soc/soc_caps.h"
-#include "esp_log.h"
 #include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
+#include <math.h>
 
-const static char *TAG = "EXAMPLE";
-
-#define ADC2_CHAN0 ADC_CHANNEL_0
+#define ADC_CHANNEL ADC_CHANNEL_6
+#define ADC_BITWIDTH ADC_BITWIDTH_12
 #define ADC_ATTEN ADC_ATTEN_DB_12
 
-static int adc_raw[1][1];
-static int voltage[1][1];
 static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
 static void adc_calibration_deinit(adc_cali_handle_t handle);
 
+adc_oneshot_unit_handle_t adc_handle;
+adc_oneshot_unit_init_cfg_t init_config = {
+    .unit_id = ADC_UNIT_1,
+    .ulp_mode = ADC_ULP_MODE_DISABLE,
+};
+adc_oneshot_chan_cfg_t config = {
+    .bitwidth = ADC_BITWIDTH,
+    .atten = ADC_ATTEN,
+};
+
+int adc_raw;
+int voltage;
+
 void sample()
-{
-    //-------------ADC Config---------------//
-    adc_oneshot_chan_cfg_t config = {
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
-        .atten = ADC_ATTEN,
-    };
+{   
+    adc_oneshot_new_unit(&init_config, &adc_handle);
 
-    //-------------ADC2 Init---------------//
-    adc_oneshot_unit_handle_t adc2_handle;
-    adc_oneshot_unit_init_cfg_t init_config2 = {
-        .unit_id = ADC_UNIT_2,
-        .ulp_mode = ADC_ULP_MODE_DISABLE,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &adc2_handle));
+    adc_oneshot_config_channel(adc_handle, ADC_CHANNEL, &config);
 
-    //-------------ADC2 Calibration Init---------------//
-    adc_cali_handle_t adc2_cali_handle = NULL;
-    bool do_calibration2 = adc_calibration_init(ADC_UNIT_2, ADC2_CHAN0, ADC_ATTEN, &adc2_cali_handle);
+    adc_cali_handle_t adc_cali_handle = NULL;
+    adc_calibration_init(ADC_UNIT_1, ADC_CHANNEL, ADC_ATTEN, &adc_cali_handle);
 
-    //-------------ADC2 Config---------------//
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, ADC2_CHAN0, &config));
-
-    ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, ADC2_CHAN0, &adc_raw[0][0]));
-    //ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_2 + 1, EXAMPLE_ADC2_CHAN0, adc_raw[0][0]);
-    if (do_calibration2) {
-        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc2_cali_handle, adc_raw[0][0], &voltage[0][0]));
-        //ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_2 + 1, EXAMPLE_ADC2_CHAN0, voltage[0][0]);
-    }
-
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc2_handle));
-    if (do_calibration2) {
-        adc_calibration_deinit(adc2_cali_handle);
-    }
+    adc_oneshot_read(adc_handle, ADC_CHANNEL, &adc_raw);
+    
+    adc_cali_raw_to_voltage(adc_cali_handle, adc_raw, &voltage);
+    
+    adc_oneshot_del_unit(adc_handle);
+    
+    adc_calibration_deinit(adc_cali_handle);
 }
 
 void app_main(void)
 {
+
     while (1) {
         sample();
-        printf("raw: %d\n", adc_raw[0][0]);
-        printf("cali: %d\n", voltage[0][0]);
+        printf("raw: %d\n", adc_raw);
+        printf("cali: %d\n", voltage);
+
+        int temp = ((10.888 - sqrt( pow(-10.888, 2) + 4 * 0.00347 * (1777.3 - voltage))) / (2 * -0.00347)) + 30;
+
+        printf("temp: %d\n", temp);
+
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
-
 
 /*---------------------------------------------------------------
         ADC Calibration
@@ -77,7 +74,7 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
 
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
     if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+        printf("calibration scheme version is %s\n", "Curve Fitting");
         adc_cali_curve_fitting_config_t cali_config = {
             .unit_id = unit,
             .chan = channel,
@@ -93,7 +90,7 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
 
 #if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
     if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+        printf("calibration scheme version is %s\n", "Line Fitting");
         adc_cali_line_fitting_config_t cali_config = {
             .unit_id = unit,
             .atten = atten,
@@ -108,11 +105,11 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
 
     *out_handle = handle;
     if (ret == ESP_OK) {
-        ESP_LOGI(TAG, "Calibration Success");
+        printf("Calibration Success\n");
     } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
-        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
+        printf("eFuse not burnt, skip software calibration\n");
     } else {
-        ESP_LOGE(TAG, "Invalid arg or no memory");
+        printf("Invalid arg or no memory\n");
     }
 
     return calibrated;
@@ -121,11 +118,11 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
 static void adc_calibration_deinit(adc_cali_handle_t handle)
 {
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
+    printf("deregister %s calibration scheme\n", "Curve Fitting");
     ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
 
 #elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
+    printf("deregister %s calibration scheme\n", "Line Fitting");
     ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
 #endif
 }
