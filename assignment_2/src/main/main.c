830263918a6f6037775c24d70fc10816f753a697
Casper Fenger Jensen <43348526+Vimian@users.noreply.github.com>
2024-05-07 13:34:25 +0200

    Save

--- assignment_2/src/main/main.c
+++ assignment_2/src/main/main.c
@@ -7,11 +7,26 @@
 #include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
+
 #include <math.h>
+
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "mqtt_client.h"
 
 #define ADC_CHANNEL ADC_CHANNEL_6
 #define ADC_BITWIDTH ADC_BITWIDTH_12
 #define ADC_ATTEN ADC_ATTEN_DB_12
+
+#define WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
+#define WIFI_SAE_MODE WPA3_SAE_PWE_BOTH
+#define H2E_IDENTIFIER ""
+#define MAXIMUM_RETRY 5
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
 
 #define WIFI_SSID CONFIG_WIFI_SSID
 #define WIFI_PASS CONFIG_WIFI_PASS
@@ -19,6 +34,10 @@
 #define MQTT_COMMAND_TOPIC CONFIG_MQTT_COMMAND_TOPIC
 #define MQTT_RESPONSE_TOPIC CONFIG_MQTT_RESPONSE_TOPIC
 
+
+/*---------------------------------------------------------------
+        Temperature
+---------------------------------------------------------------*/
 static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
 static void adc_calibration_deinit(adc_cali_handle_t handle);
 
@@ -53,10 +72,140 @@ void sample()
     adc_calibration_deinit(adc_cali_handle);
 }
 
+/*---------------------------------------------------------------
+        WiFi
+---------------------------------------------------------------*/
+static EventGroupHandle_t s_wifi_event_group;
+static int s_retry_num = 0;
+static esp_ip4_addr_t ip;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            printf("retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        printf("connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        printf("got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        ip = event->ip_info.ip;
+
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = WIFI_SCAN_AUTH_MODE_THRESHOLD,
+            .sae_pwe_h2e = WIFI_SAE_MODE,
+            .sae_h2e_identifier = H2E_IDENTIFIER,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+/*---------------------------------------------------------------
+        MQTT
+---------------------------------------------------------------*/
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+
+    switch ((esp_mqtt_event_id_t)event_id) {
+        case MQTT_EVENT_CONNECTED:
+            printf("MQTT_EVENT_CONNECTED\n");
+            //msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+            msg_id = esp_mqtt_client_subscribe(client, MQTT_COMMAND_TOPIC, 0);
+            printf("sent subscribe successful, msg_id=%d\n", msg_id);
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            printf("MQTT_EVENT_DISCONNECTED\n");
+            break;
+        case MQTT_EVENT_DATA:
+            printf("MQTT_EVENT_DATA\n");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+            break;
+        case MQTT_EVENT_ERROR:
+            printf("MQTT_EVENT_ERROR\n");
+            if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+                printf("Last errno string (%s)\n", strerror(event->error_handle->esp_transport_sock_errno));
+            }
+            break;
+        default:
+            printf("Unhandled event: %d\n", event->event_id);
+            break;
+    }
+}
+
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = MQTT_BROKER,
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
+
+/*---------------------------------------------------------------
+        Main
+---------------------------------------------------------------*/
 void app_main(void)
 {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-    while (1) {
+    wifi_init_sta();
+
+    mqtt_app_start();
+
+    /*while (1) {
         sample();
         printf("raw: %d\n", adc_raw);
         printf("cali: %d\n", voltage);
@@ -64,15 +213,11 @@ void app_main(void)
         int temp = ((10.888 - sqrt( pow(-10.888, 2) + 4 * 0.00347 * (1777.3 - voltage))) / (2 * -0.00347)) + 30;
 
         printf("temp: %d\n", temp);
-
-        printf("WIFI_SSID: %s\n", WIFI_SSID);
-        printf("WIFI_PASS: %s\n", WIFI_PASS);
-        printf("MQTT_BROKER: %s\n", MQTT_BROKER);
-        printf("MQTT_COMMAND_TOPIC: %s\n", MQTT_COMMAND_TOPIC);
-        printf("MQTT_RESPONSE_TOPIC: %s\n", MQTT_RESPONSE_TOPIC);
+        printf("got ip: " IPSTR, IP2STR(&ip));
+        printf("\n");
 
         vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    }*/
 }
 
 /*---------------------------------------------------------------
